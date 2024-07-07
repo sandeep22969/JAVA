cc106502ba81485812c9fca8133745b12499c108
sandeep22969 <94220217+sandeep22969@users.noreply.github.com>
2024-07-07 11:35:56 +0530

    Assigning values to  Variables.java

--- Assigning values to  Variables.java
+++ Assigning values to  Variables.java
@@ -3,14 +3,10 @@ class Hello
     public static void main(String args[])
     {
         int num1=10;
-        int num2=5;
-        int add = num1+num2;
-        int sub = num1-num2;
-        int mul = num1*num2;
-        int div = num1/num2;
-        System.out.println(add);
-        System.out.println(sub);
-        System.out.println(mul);
-        System.out.println(div);
+        Float marks=6.5f;
+        String user="sandeep";
+        System.out.println(num1);
+        System.out.println(marks);
+        System.out.println(user);
     }
 }
