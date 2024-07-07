0f50bf0b706d359ad5d0a698eb788529888448ce
sandeep22969 <94220217+sandeep22969@users.noreply.github.com>
2024-07-07 11:09:07 +0530

    Type Casting.java

---
+++ Type Casting.java
@@ -0,0 +1,14 @@
+class Hello
+{
+    public static void main(String a[])
+    {
+        int b=257;
+        byte k=(byte)b;
+
+        float f=5.6f;
+        int t=(int)f;
+
+        System.out.println(k);
+        System.out.println(t);
+    }
+}
