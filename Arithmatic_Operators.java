d5c273b98c398248953c2e7d9f8d6f22d7565e30
sandeep22969 <94220217+sandeep22969@users.noreply.github.com>
2024-07-07 12:03:43 +0530

    Create Arithmatic_Operators.java

---
+++ Arithmatic_Operators.java
@@ -0,0 +1,25 @@
+class Hello
+{
+    public static void main(String args[])
+    {
+        int num1=7;
+        int num2=5; 
+        int num3=10;
+        int num4=20;
+        int add=num1+num2;  //Addition
+        int sub=num1-num2;  //Subtraction
+        int mul=num1*num2;  //Multiplication
+        int quo=num1/num2;  //Quotient   
+        int rem=num1%num2;  //Remainder
+        num3++;             //Increment
+        num4--;             //Decrement
+        
+        System.out.println(add); 
+        System.out.println(sub);
+        System.out.println(mul);
+        System.out.println(quo);
+        System.out.println(rem);
+        System.out.println(num3);
+        System.out.println(num4);
+    }
+}
