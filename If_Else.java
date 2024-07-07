8fea1579fd84039a62defb17710ebf83e66272e4
sandeep22969 <94220217+sandeep22969@users.noreply.github.com>
2024-07-07 22:59:22 +0530

    Create If_Else.java

---
+++ If_Else.java
@@ -0,0 +1,13 @@
+class Hello
+{
+    public static void main(String args[])
+    {
+        int x=7;
+        int y=5;
+        if(x>y)
+            System.out.println(x);      
+        else
+            System.out.println(y);  
+        
+    }
+}
