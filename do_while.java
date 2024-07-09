644013174808f52552e2c4df755d1f9718a450a7
sandeep22969 <94220217+sandeep22969@users.noreply.github.com>
2024-07-09 19:39:48 +0530

    Create do_while.java

---
+++ do_while.java
@@ -0,0 +1,12 @@
+public class Hello
+{
+    public static void main(String args[])
+    {
+        int i=5;
+        do
+        {
+            System.out.println("Hi "+i);
+            i++;
+        }while(i<=4);   
+    }
+}
