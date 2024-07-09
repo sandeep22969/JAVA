eefb0b56cf3a2634aaec4b82bb548e0e70cc3bbc
sandeep22969 <94220217+sandeep22969@users.noreply.github.com>
2024-07-09 19:21:45 +0530

    Create While_Loop.java

---
+++ While_loop.java
@@ -0,0 +1,13 @@
+class Hello
+{
+    public static void main(String args[])
+    {
+        int i=1;
+        while(i<=4)
+        {
+            System.out.println("Hi "+i);
+            i++;
+        }    
+            System.out.println("Bye "+ i);//Failed statement
+    }
+}
