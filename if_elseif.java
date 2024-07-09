f6ae93e007873e8f7a9218d1dee5dee7996a288b
sandeep22969 <94220217+sandeep22969@users.noreply.github.com>
2024-07-09 09:11:24 +0530

    Create if_elseif.java

---
+++ if_elseif.java
@@ -0,0 +1,15 @@
+class Hello
+{
+    public static void main(String args[])
+    {
+        int x=7;
+        int y=5;
+        int z=9;
+        if(x>y && x>z)
+            System.out.println(x);      
+        else if(y>z)
+            System.out.println(y);  
+        else
+            System.out.println(z);       
+    }
+}
