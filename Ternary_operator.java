037e442ae2ef8fe4f5630153a7bc6bd1b7ceeccf
sandeep22969 <94220217+sandeep22969@users.noreply.github.com>
2024-07-09 09:38:23 +0530

    Create Ternary_operator.java

---
+++ Ternary_operator.java
@@ -0,0 +1,10 @@
+class Hello
+{
+    public static void main(String args[])
+    {
+        int n=6;
+        int result=0;
+        result =n%2==0?10:20;
+        System.out.println(result);       
+    }
+}
