15b989612557eb0b170dead74aa7d8e01aafc9f3
sandeep22969 <94220217+sandeep22969@users.noreply.github.com>
2024-07-09 09:35:55 +0530

    Create Even_Odd.java

---
+++ Even_Odd.java
@@ -0,0 +1,12 @@
+class Hello
+{
+    public static void main(String args[])
+    {
+        int x=8;
+
+        if(x%2==0)
+            System.out.println("Even");  
+        else
+            System.out.println("Odd");       
+    }
+}
